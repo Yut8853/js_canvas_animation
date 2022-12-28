34cf99f55afcdae5ee9dfd4920d522c7593368c2
yk0611 <hello@junkbranding.com>
2022-12-28 12:59:28 +0900

    template

---
+++ template/app.js
@@ -0,0 +1,20 @@
+let canvas, ctx
+
+setup()
+draw()
+
+window.addEventListener('resize', () => {
+  setup()
+})
+
+function setup() {
+  canvas = document.querySelector('#canvas')
+  ctx = canvas.getContext('2d')
+  canvas.style.backgroundColor = '#222'
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
+
+function draw() {
+
+}
